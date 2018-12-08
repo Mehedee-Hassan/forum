2f7b3a90ec36c9a1710a2778ab6857ee263afdf6
Mehedee Hassan <razin.meha@gmail.com>
2018-12-08 16:07:31 +0600

    beautify

--- resources/views/thread/single.blade.php
+++ resources/views/thread/single.blade.php
@@ -31,51 +31,55 @@
             {{--comment--}}
             <h4>{{ $comment->body }}</h4>
             <lead> <strong>-{{ $comment->user->name }}</strong></lead>
-
-            <div class="action">
-{{--                <a href="{{route('comment.update',$comment->id)}}" class="btn btn-info btn-xs">Edit</a>--}}
-
-                <a class="btn btn-primary btn-xs" data-toggle="modal" href="#{{ $comment->id }}">edit</a>
-
-
-                <div class="modal fade" id="{{ $comment->id }}">
-                	<div class="modal-dialog">
-                		<div class="modal-content">
-                			<div class="modal-header">
-                				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                				<h4 class="modal-title">Edit Comment</h4>
-                			</div>
-                			<div class="modal-body">
-                                <form action="{{route('comment.update',$comment->id)}}" method="post" role="form">
-
-                                    {{ csrf_field() }}
-                                    {{ method_field('put') }}
-
-                                    <legend>Comment</legend>
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" name="body" id="body" placeholder="comment..."  value="{{ $comment->body }}"/>
-                                    </div>
-
-
-                                    <button type="submit" class="btn btn-primary">Comment</button>
-                                </form>
-                			</div>
-                			<div class="modal-footer">
-                				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                				{{--<button type="button" class="btn btn-primary">Save changes</button>--}}
-                			</div>
-                		</div><!-- /.modal-content -->
-                	</div><!-- /.modal-dialog -->
-                </div><!-- /.modal -->
-
-                <form action="{{ route('comment.destroy',$comment->id) }}" method="post" class="inline-it">
-                    {{ csrf_field() }}
-                    {{method_field('DELETE')}}
-                    <input class="btn btn-xs btn-danger" type="submit" value="Delete"/>
-                </form>
-
-            </div>
             <br>
+            <br>
+
+            @if(auth()->user()->id == $comment->user->id)
+                <div class="action">
+    {{--                <a href="{{route('comment.update',$comment->id)}}" class="btn btn-info btn-xs">Edit</a>--}}
+
+                    <a class="btn btn-primary btn-xs" data-toggle="modal" href="#{{ $comment->id }}">edit</a>
+
+
+                    <div class="modal fade" id="{{ $comment->id }}">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                    <h4 class="modal-title">Edit Comment</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="{{route('comment.update',$comment->id)}}" method="post" role="form">
+
+                                        {{ csrf_field() }}
+                                        {{ method_field('put') }}
+
+                                        <legend>Comment</legend>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" name="body" id="body" placeholder="comment..."  value="{{ $comment->body }}"/>
+                                        </div>
+
+
+                                        <button type="submit" class="btn btn-primary">Comment</button>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    {{--<button type="button" class="btn btn-primary">Save changes</button>--}}
+                                </div>
+                            </div><!-- /.modal-content -->
+                        </div><!-- /.modal-dialog -->
+                    </div><!-- /.modal -->
+
+                    <form action="{{ route('comment.destroy',$comment->id) }}" method="post" class="inline-it">
+                        {{ csrf_field() }}
+                        {{method_field('DELETE')}}
+                        <input class="btn btn-xs btn-danger" type="submit" value="Delete"/>
+                    </form>
+
+                </div>
+            @endif
+
 
             {{--reply to comment--}}
                 @foreach($comment->comments as $reply)
@@ -83,54 +87,60 @@
                         <p>{{$reply->body}}</p>
                         --<lead>{{ $reply->user->name }}</lead>
 
-
-                        <div class="action">
-
-                            <a class="btn btn-primary btn-xs" data-toggle="modal" href="#{{ $reply->id }}">edit</a>
+                        <br>
+                        <br>
 
 
-                            <div class="modal fade" id="{{ $reply->id }}">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                            <h4 class="modal-title">Edit Reply</h4>
-                                        </div>
-                                        <div class="modal-body">
-                                            <form action="{{route('comment.update',$reply->id)}}" method="post" role="form">
+                        @if(auth()->user()->id == $comment->user->id)
+                            <div class="action">
 
-                                                {{ csrf_field() }}
-                                                {{ method_field('put') }}
-
-                                                <legend>Save</legend>
-                                                <div class="form-group">
-                                                    <input type="text" class="form-control" name="body" id="body" placeholder="comment..."  value="{{ $reply->body }}"/>
-                                                </div>
+                                <a class="btn btn-primary btn-xs" data-toggle="modal" href="#{{ $reply->id }}">Edit reply</a>
 
 
-                                                <button type="submit" class="btn btn-primary">Reply</button>
-                                            </form>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                            {{--<button type="button" class="btn btn-primary">Save changes</button>--}}
-                                        </div>
-                                    </div><!-- /.modal-content -->
-                                </div><!-- /.modal-dialog -->
-                            </div><!-- /.modal -->
+                                <div class="modal fade" id="{{ $reply->id }}">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                                <h4 class="modal-title">Edit Reply</h4>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form action="{{route('comment.update',$reply->id)}}" method="post" role="form">
 
-                            <form action="{{ route('comment.destroy',$reply->id) }}" method="post" class="inline-it">
-                                {{ csrf_field() }}
-                                {{method_field('DELETE')}}
-                                <input class="btn btn-xs btn-danger" type="submit" value="Delete"/>
-                            </form>
+                                                    {{ csrf_field() }}
+                                                    {{ method_field('put') }}
 
-                        </div>
+                                                    <legend>Save</legend>
+                                                    <div class="form-group">
+                                                        <input type="text" class="form-control" name="body" id="body" placeholder="comment..."  value="{{ $reply->body }}"/>
+                                                    </div>
+
+
+                                                    <button type="submit" class="btn btn-primary">Reply</button>
+                                                </form>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                {{--<button type="button" class="btn btn-primary">Save changes</button>--}}
+                                            </div>
+                                        </div><!-- /.modal-content -->
+                                    </div><!-- /.modal-dialog -->
+                                </div><!-- /.modal -->
+
+                                <form action="{{ route('comment.destroy',$reply->id) }}" method="post" class="inline-it">
+                                    {{ csrf_field() }}
+                                    {{method_field('DELETE')}}
+                                    <input class="btn btn-xs btn-danger" type="submit" value="Delete Reply"/>
+                                </form>
+
+                            </div>
+                        @endif
 
 
                     </div>
                 @endforeach
-            <br>
+
+
             {{--reply form--}}
             <div  class="reply-form">
 
@@ -147,7 +157,9 @@
                 </form>
                 <br>
 
+
             </div>
+            <hr/>
         @endforeach
     </div>
 
